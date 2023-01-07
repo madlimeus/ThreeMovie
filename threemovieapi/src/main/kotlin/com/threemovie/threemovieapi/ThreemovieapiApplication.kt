14b7156be59fcd35f709b9ed8e0c2d495d46cf36
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-01-07 20:43:57 +0900

    main
    main

---
+++ threemovieapi/src/main/kotlin/com/threemovie/threemovieapi/ThreemovieapiApplication.kt
@@ -0,0 +1,11 @@
+package com.threemovie.threemovieapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ThreemovieapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ThreemovieapiApplication>(*args)
+}
---
+++ threemovieapi/src/test/kotlin/com/threemovie/threemovieapi/ThreemovieapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.threemovie.threemovieapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ThreemovieapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
