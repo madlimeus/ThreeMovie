e48bd29a3a151060fd0c091030249f9c6499a142
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-04-22 17:21:11 +0900

    주소 변경
    주소 변경

--- threemovieweb/src/apollo/client.ts
+++ threemovieweb/src/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink } from '@apollo/client';
 import { cache } from './cache';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:8080/graphql',
+    uri: 'http://moviethree.synologe.me:8080/graphql',
 });
 
 export const client = new ApolloClient({
