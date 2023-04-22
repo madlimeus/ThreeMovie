70c55c37bfd67e74e2464f33328b433b4e370399
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-04-23 02:34:07 +0900

    graphql 주소 통합
    graphql 주소 통합

--- threemovieweb/src/apollo/client.ts
+++ threemovieweb/src/apollo/client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink } from '@apollo/client';
 import { cache } from './cache';
 
 const httpLink = createHttpLink({
-    uri: 'http://moviethree.synologe.me/graphql',
+    uri: 'http://moviethree.synologe.me/api/graphql',
 });
 
 export const client = new ApolloClient({
